597bc8930acb9b577ba7de5ca82e289b5a507470
Karina <verworrenne@yandex.ru>
2021-05-26 14:42:49 +0300

    updated ds

--- create_dataset.py
+++ create_dataset.py
@@ -18,12 +18,11 @@ COLUMNS = [
     'координата x середины стены',
     'координата y середины стены',
     'координата x конца стены',
-    'координата y конца стены',
-    'метка'
+    'координата y конца стены'
 ]
 
 
-def add_new_point_to_csv(filename, map_name, n_friends, n_enemies, friends_coord, enemies_coord, wall_exists, wall_begins, wall_middle, wall_ends, label):
+def add_new_point_to_csv(filename, map_name, n_friends, n_enemies, friends_coord, enemies_coord, wall_exists, wall_begins, wall_middle, wall_ends):
     """Записывает точку данных в табличку, которая находится в .csv файлике."""
     x_friends_coord, y_friends_coord = friends_coord
     x_enemies_coord, y_enemies_coord = enemies_coord
@@ -44,8 +43,7 @@ def add_new_point_to_csv(filename, map_name, n_friends, n_enemies, friends_coord
         'координата x середины стены': x_wall_middle,
         'координата y середины стены': y_wall_middle,
         'координата x конца стены': x_wall_ends,
-        'координата y конца стены': y_wall_ends,
-        'метка': label
+        'координата y конца стены': y_wall_ends
     }
     df = pd.read_csv(filename)
     df = df.append(new_row, ignore_index=True)
@@ -57,8 +55,7 @@ def main():
     table.to_csv(TABLE_FILE_NAME, index=False)  # сохраняем эту табличку в .csv файл
 
     # записываем экземпляры точек данных в табличку
-    #add_new_point_to_csv(TABLE_FILE_NAME, '2m2mFOX', 2, 2, (1, 1), (10, 1), 0, (2, 2), (4, 2), (8, 2), 'q-learning')
-
+    #add_new_point_to_csv(TABLE_FILE_NAME, '2m2mFOX', 2, 2, (1, 1), (10, 1), 0, (2, 2), (4, 2), (8, 2))
 
 
 if __name__ == '__main__':
